f60e36c0d5732e0ef4e6dfa45e5ce24cf5058b4e
arkadiuszch <arkadiusz.cholewaa@gmail.com>
2018-05-18 17:38:05 +0200

    Rating: edit, delete, details added

--- ASP_NET_MVC_5/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ ASP_NET_MVC_5/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -98,7 +98,7 @@ namespace MvcMovie.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
